76275175f013f9d54eeb7ee30a0c3f0bcd6f0a51
AjithS10 <ajithsnair10@gmail.com>
2024-06-24 14:57:27 +0530

    server main.js handle catch & res interceptor hanlde added

--- server-apis/src/app.module.ts
+++ server-apis/src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BlockchainModule } from './blockchain/blockchain.module';
-import { CarGarageModule } from './car-garage/car-garage.module';
-import { MotocycleGarageModule } from './motocycle-garage/motocycle-garage.module';
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { ConfigModule } from "@nestjs/config";
+import { ScheduleModule } from "@nestjs/schedule";
+import { AppService } from "./app.service";
+import { BlockchainModule } from "./blockchain/blockchain.module";
+import { CarGarageModule } from "./car-garage/car-garage.module";
+import { MotocycleGarageModule } from "./motocycle-garage/motocycle-garage.module";
 @Module({
-  imports: [BlockchainModule, CarGarageModule, MotocycleGarageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    BlockchainModule,
+    CarGarageModule,
+    MotocycleGarageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server-apis/src/main.ts
+++ server-apis/src/main.ts
@@ -1,8 +1,45 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory, Reflector } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { useContainer } from "class-validator";
+import { CatchFilter } from "./catch.interceptor";
+import { ResInterceptor } from "./res.interceptor";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Enable CORS with specific options
+  app.enableCors({
+    // origin: process.env.APP_CORS_ORIGINS.split(','),
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+  app.use(helmet());
+  app.setGlobalPrefix("api/v1");
+
+  // Apply strict validation and transformation to incoming requests globally.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Transform incoming data according to DTO class.
+      whitelist: true, // Only allow properties defined in the DTO.
+      forbidUnknownValues: true, // Reject unknown properties in the payload.
+      forbidNonWhitelisted: true, // Reject properties not defined in DTO.
+      stopAtFirstError: true, // Stop validation at first error encountered.
+    })
+  );
+
+  useContainer(app.select(AppModule), {
+    fallbackOnErrors: true, // If an error occurs while resolving dependencies, fallback to default behavior.
+  });
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new ResInterceptor());
+  app.useGlobalFilters(new CatchFilter());
+
+  app.useGlobalFilters();
+
+  await app.listen(process.env.SERVER_PORT || 4000);
 }
 bootstrap();
