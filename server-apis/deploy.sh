cfe0a6175a51b6f410e472c60c4e120c7e967345
AjithS10 <ajithsnair10@gmail.com>
2024-07-11 17:21:30 +0530

    deploy script updated with logging the steps

--- server-apis/deploy.sh
+++ server-apis/deploy.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-# Define variables
+# Define variables 
+echo "<< ========== Define variables ============ >>"
 PROCESS_NAME="server_api"
 PORT=4000
 
-# Step 1: Pull latest changes from Git repository
-echo "Pulling latest changes from Git repository..."
+# Step 1: Pull latest changes from Git repository 
+echo "<< ==========  Pulling latest changes from Git repository... ============ >>"
 git pull
 
-# Step 2: Clean install npm dependencies
-echo "Installing npm dependencies..."
+# Step 2: Clean install npm dependencies 
+echo "<< ========== Installing npm dependencies... ============ >>"
 npm ci
 
-# Step 3: Build the project
-echo "Building the project..."
+# << Step 3: Build the project
+echo "<< ========== Building the project... ============ >>"
 npm run build
 
-# Step 4: Reload or start the PM2 process
-echo "Reloading or starting the PM2 process..."
+# Step 4: Starting migration
+echo "<< ========== Starting migration... ============ >>"
+npm run typeorm:run-migrations
+
+
+# Step 5: Reload or start the PM2 process 
+echo "<< ========== Reloading or starting the PM2 process... ============ >>"
 if pm2 reload "$PROCESS_NAME"; then
     echo "$PROCESS_NAME reloaded"
     pm2 status
