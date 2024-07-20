5d08fa892bb6146164fe465a18b15c58a6e60c83
AjithS10 <ajithsnair10@gmail.com>
2024-07-20 14:49:05 +0530

    feat(patients): create patients asset smart contract added | contract initial setup

--- MediLedger-chaincodes/patients-chaincode/lib/patients.contract.js
+++ MediLedger-chaincodes/patients-chaincode/lib/patients.contract.js
@@ -13,6 +13,25 @@ class PatientContract extends Contract {
         console.log(message);
         return message;
     }
+
+    async CreateAsset(ctx, patientData) {
+        const exists = await this.AssetExists(ctx, patientData.patient_id);
+        if (exists) {
+            throw new Error(`The asset ${patientData.patient_id} already exists`);
+        }
+        let indexName = 'Zoro123';
+        let asset = {
+            patient_id: patientData.patient_id,
+            name: patientData.name,
+            email: patientData.email,
+            phone: patientData.phone,
+            dob: patientData.dob,
+            blood_group: patientData.blood_group,
+        };
+        await ctx.stub.putState(assetID, Buffer.from(JSON.stringify(asset)));
+        let colorNameIndexKey = await ctx.stub.createCompositeKey(indexName, [asset.name, asset.patient_id]);
+        await ctx.stub.putState(colorNameIndexKey, Buffer.from('\u0000'));
+    }
 }
 
 module.exports = PatientContract;
