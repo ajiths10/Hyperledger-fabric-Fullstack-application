37ef895b9a389ee01bdb5b2ab8cf505dce47e783
AjithS10 <ajithsnair10@gmail.com>
2024-07-20 15:01:35 +0530

    feat(response_wrapper): sucess and error response wrapper common functions added

--- server-apis/src/response_wrapper/response_wrapper.service.ts
+++ server-apis/src/response_wrapper/response_wrapper.service.ts
@@ -1,6 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { errorResponse, successResponse } from './types/response_wrapper.types';
 
 @Injectable()
 export class ResponseWrapperService {
     constructor() {}
+
+    /**
+     * Generates a success response with optional data.
+     * @param {string} message - The success message.
+     * @param {any} [data] - Optional data to include in the response.
+     * @returns {{status: boolean, message: string, data: any}} The success response object.
+     */
+    _successResponse(message: string, data?: any): successResponse {
+        return {
+            status: true,
+            message: message,
+            data: data,
+        };
+    }
+
+    /**
+     * Generates an error response with a message.
+     * @param {string} message - The error message.
+     * @returns {{status: boolean, message: string}} The error response object.
+     */
+    _errorResponse(message: string): errorResponse {
+        return {
+            status: false,
+            message: message,
+        };
+    }
 }
---
+++ server-apis/src/response_wrapper/types/response_wrapper.types.ts
@@ -0,0 +1,10 @@
+export type successResponse = {
+    status: boolean;
+    message: string;
+    data: any;
+};
+
+export type errorResponse = {
+    status: boolean;
+    message: string;
+};
