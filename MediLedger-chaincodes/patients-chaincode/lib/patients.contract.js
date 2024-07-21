585d02ead7aa3f85ea7990bf8b461aace02adcc5
AjithS10 <ajithsnair10@gmail.com>
2024-07-21 10:31:51 +0530

    feat(patients): process query results internal function added with jsDoc

--- MediLedger-chaincodes/patients-chaincode/lib/patients.contract.js
+++ MediLedger-chaincodes/patients-chaincode/lib/patients.contract.js
@@ -41,6 +41,46 @@ class PatientContract extends Contract {
         let assetState = await ctx.stub.getState(assetName);
         return assetState && assetState.length > 0;
     }
+
+    /**
+     * Processes results from an iterator, handling either historical or current data.
+     * @function processQueryResults
+     * @param {Object} iterator - An iterator providing access to the results.
+     * @param {boolean} isHistory - A flag indicating if the data is historical.
+     * @returns {Promise<Array<Object>>} A promise that resolves to an array of results.
+     */
+    async _processQueryResults(iterator, isHistory) {
+        let allResults = [];
+        let res = await iterator.next();
+        while (!res.done) {
+            if (res.value && res.value.value.toString()) {
+                let jsonRes = {};
+                console.log(res.value.value.toString('utf8'));
+                if (isHistory && isHistory === true) {
+                    jsonRes.TxId = res.value.txId;
+                    jsonRes.Timestamp = res.value.timestamp;
+                    try {
+                        jsonRes.Value = JSON.parse(res.value.value.toString('utf8'));
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Value = res.value.value.toString('utf8');
+                    }
+                } else {
+                    jsonRes.Key = res.value.key;
+                    try {
+                        jsonRes.Record = JSON.parse(res.value.value.toString('utf8'));
+                    } catch (err) {
+                        console.log(err);
+                        jsonRes.Record = res.value.value.toString('utf8');
+                    }
+                }
+                allResults.push(jsonRes);
+            }
+            res = await iterator.next();
+        }
+        iterator.close();
+        return allResults;
+    }
 }
 
 module.exports = PatientContract;
