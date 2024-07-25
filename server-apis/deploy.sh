1265271ced39bda026fb9c7b2ddb2e2811cdda18
AjithS10 <ajithsnair10@gmail.com>
2024-07-25 11:33:14 +0530

    chore(deploy): shell script updated with pm2 process stoping comment added and logs message updated

--- server-apis/deploy.sh
+++ server-apis/deploy.sh
@@ -5,24 +5,28 @@ echo "<< ========== Define variables ============ >>"
 PROCESS_NAME="server_api"
 PORT=4000
 
-# Step 1: Pull latest changes from Git repository 
+# Step 1: Stop PM2 running process
+echo "<< ==========  Stoping PM2 running process $PROCESS_NAME ============ >>"
+pm2 stop $PROCESS_NAME
+
+# Step 2: Pull latest changes from Git repository 
 echo "<< ==========  Pulling latest changes from Git repository... ============ >>"
 git pull
 
-# Step 2: Clean install npm dependencies 
+# Step 3: Clean install npm dependencies 
 echo "<< ========== Installing npm dependencies... ============ >>"
 npm ci
 
-# << Step 3: Build the project
+# Step 4: Build the project
 echo "<< ========== Building the project... ============ >>"
 npm run build
 
-# Step 4: Starting migration
+# Step 5: Starting migration
 echo "<< ========== Starting migration... ============ >>"
 npm run typeorm:run-migrations
 
 
-# Step 5: Reload or start the PM2 process 
+# Step 6: Reload or start the PM2 process 
 echo "<< ========== Reloading or starting the PM2 process... ============ >>"
 if pm2 reload "$PROCESS_NAME"; then
     echo "$PROCESS_NAME reloaded"
