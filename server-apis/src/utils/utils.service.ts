ebf1d26ada0adeaa0e988aeeb749ee07b4db82b8
AjithS10 <ajithsnair10@gmail.com>
2024-07-31 10:19:17 +0530

    chore(patients): buffer to string common function

--- server-apis/src/utils/utils.service.ts
+++ server-apis/src/utils/utils.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UtilsService {
@@ -6,5 +6,16 @@ export class UtilsService {
 
     _envOrDefault(key: string, defaultValue: string): string {
         return process.env[key] || defaultValue;
+    }
+
+    _prettyJSONString(resultBytes: AllowSharedBufferSource) {
+        try {
+            const utf8Decoder = new TextDecoder();
+            const resultJson = utf8Decoder.decode(resultBytes);
+            const result = JSON.parse(resultJson);
+            return result;
+        } catch (e) {
+            return 'Err:Invalid JSON string';
+        }
     }
 }
