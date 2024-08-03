5b6f61be1eff08a03c12583c6b372a5c1adb5ceb
AjithS10 <ajithsnair10@gmail.com>
2024-08-03 10:29:04 +0530

    chore(patients): create asset API response updated with buffer to object wrapper

--- server-apis/src/patients/patients.service.ts
+++ server-apis/src/patients/patients.service.ts
@@ -49,8 +49,9 @@ export class PatientsService implements OnModuleInit {
     async create(body: CreatePatientDto) {
         try {
             console.log('\n--> Submit Transaction: CreateAsset');
-            let response = await this.Contract.submitTransaction('CreateAsset', 'Asset_003', body.name, body.email, body.phone, body.dob, body.blood_group);
+            let resultBytes = await this.Contract.submitTransaction('CreateAsset', 'Asset_005', body.name, body.email, body.phone, body.dob, body.blood_group);
             console.log('*** Transaction committed successfully ***');
+            const response = this.utilsService._prettyBufferToObject(resultBytes);
             return this.responseWrapperService._successResponse('Transaction committed successfully: "Patient created successfully!"', response);
         } catch (error) {
             console.log(`******** FAILED to return an error ***********`, error.message);
