ee85bc2ef78d79b675a587cd72ae1df4fea1195b
AjithS10 <ajithsnair10@gmail.com>
2024-08-05 10:10:29 +0530

    chore(utils): add JsDoc comments for functio to converts a byte buffer to a JSON object

--- server-apis/src/utils/utils.service.ts
+++ server-apis/src/utils/utils.service.ts
@@ -8,6 +8,13 @@ export class UtilsService {
         return process.env[key] || defaultValue;
     }
 
+    /**
+     * Converts a byte buffer to a JSON object.
+     * This function takes a buffer, decodes it using UTF-8, and then parses the decoded string into a JSON object.
+     * If the input buffer does not contain valid JSON, an error message is returned.
+     * @param {AllowSharedBufferSource} resultBytes - The byte buffer to be converted to a JSON object.
+     * @returns {Object|string} The resulting JSON object or an error message if the JSON is invalid.
+     */
     _prettyBufferToObject(resultBytes: AllowSharedBufferSource) {
         try {
             const utf8Decoder = new TextDecoder();
